9fc75af26339f9a86e9af9078c4d2d187b46fad2
nick wise <nickmwise@gmail.com>
2017-07-14 09:04:57 -0700

    Add model for members including name,email,description,externallink, and id. Add new member objects to be stored in portal component for now. soon to be moved to mock-member.ts. Update portal htmlto ngFor loop though all member.name ...

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-portal',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portal.component.css']
 })
 export class PortalComponent implements OnInit {
-
+  members: Member[] = [
+    new Memeber("james", "music production", "james@gmail.com", "www.soundcloud.com/jamesMusic", 1),
+    new Memeber("Nick Wise", "Love music production. Anything audio related", "nickmwise@gmail.com", "www.soundcloud.com/YcleptInsan", 2),
+    new Memeber("Larry", "music production and Linux Admin", "larry@gmail.com", "www.soundcloud.com/jamesMusic", 3),
+  ]
   constructor() { }
 
   ngOnInit() {
