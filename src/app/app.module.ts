e3f1398c44f503fedeba3659923bdfa1e136f42a
nick wise <nickmwise@gmail.com>
2017-07-14 09:07:30 -0700

    add new memeber detail component template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { PortalComponent } from './portal/portal.component';
+import { MemberDetailsComponent } from './member-details/member-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     AboutComponent,
-    PortalComponent
+    PortalComponent,
+    MemberDetailsComponent
   ],
   imports: [
     BrowserModule,
